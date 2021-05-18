87acf37ae5137a219a1c2710fd041f38c4f2079d
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-05-18 21:58:50 +0100

    updates

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 import express from "express";
+import db from "./database.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get("/api", (req, res) => {
-  const state = {
-    message: "Busy",
-    alert: 1,
-  };
-  res.json(state);
+  const sql = "SELECT * from status";
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json(rows[0]);
+  });
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
