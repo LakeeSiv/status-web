f4dbba21e71bbab0b41379d2d18cf06ea1adf7d5
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-05-18 21:54:03 +0100

    database

---
+++ server/database.js
@@ -0,0 +1,32 @@
+import sqlite3 from "sqlite3";
+
+const DBSOURCE = "db.sqlite";
+
+const db = new sqlite3.Database(DBSOURCE, (err) => {
+  if (err) {
+    // Cannot open database
+    console.error(err.message);
+    throw err;
+  } else {
+    console.log("Connected to the SQLite database.");
+    db.run(
+      `CREATE TABLE status (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            message TEXT, 
+            alert INT 
+            )`,
+      (err) => {
+        if (err) {
+          console.log("Table already created");
+          console.log(err);
+        } else {
+          console.log("Inserting");
+          var insert = "INSERT INTO status (message, alert) VALUES (?,?)";
+          db.run(insert, ["hi 1", 1]);
+        }
+      }
+    );
+  }
+});
+
+export default db;
