581c6eaa44e07fedf9a175350f09d719ed3172ac
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-05-23 11:04:29 +0100

    types

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,11 @@
-import express from "express";
+import express, { Application, Request, Response } from "express";
 import db from "./database";
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+const app: Application = express();
+const PORT: number | string = process.env.PORT || 5000;
 
-app.get("/api", (req, res) => {
-  const sql = "SELECT * from status";
+app.get("/api", (req: Request, res: Response) => {
+  const sql: string = "SELECT * from status";
   db.all(sql, [], (err, rows) => {
     if (err) {
       res.status(400).json({ error: err.message });
@@ -15,7 +15,7 @@ app.get("/api", (req, res) => {
   });
 });
 
-app.use((req, res) => {
+app.use((req: Request, res: Response) => {
   res.status(404).json({ error: "page does not exist" });
 });
 
