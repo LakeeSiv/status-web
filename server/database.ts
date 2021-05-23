28e1a0fe1d8bbc5521e62b31d1f3d1319906c5ec
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-05-23 11:00:53 +0100

    added types

--- server/database.ts
+++ server/database.ts
@@ -1,31 +1,29 @@
 import sqlite3 from "sqlite3";
 
-const DBSOURCE = "db.sqlite";
+const DBSOURCE: string = "db.sqlite";
+const sql: string = `CREATE TABLE status (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            message TEXT, 
+            alert INT 
+            )`;
 
-const db = new sqlite3.Database(DBSOURCE, (err) => {
+const db: sqlite3.Database = new sqlite3.Database(DBSOURCE, (err) => {
   if (err) {
     // Cannot open database
     console.error(err.message);
     throw err;
   } else {
     console.log("Connected to the SQLite database.");
-    db.run(
-      `CREATE TABLE status (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            message TEXT, 
-            alert INT 
-            )`,
-      (err) => {
-        if (err) {
-          console.log("Table already created");
-          //   console.log(err);
-        } else {
-          //   console.log("Inserting");
-          //   var insert = "INSERT INTO status (message, alert) VALUES (?,?)";
-          //   db.run(insert, ["hi 1", 1]);
-        }
+    db.run(sql, (err) => {
+      if (err) {
+        console.log("Table already created");
+        //   console.log(err);
+      } else {
+        //   console.log("Inserting");
+        //   var insert = "INSERT INTO status (message, alert) VALUES (?,?)";
+        //   db.run(insert, ["hi 1", 1]);
       }
-    );
+    });
   }
 });
 
