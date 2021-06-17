c885be6bd60b377a1b9389028aea385959dfbcd1
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-06-17 20:15:37 +0100

    getData

--- server/database.ts
+++ server/database.ts
@@ -1,16 +1,34 @@
-import { Pool } from "pg";
+import { Pool, QueryResult } from "pg";
 import { config } from "dotenv";
 config();
 
 const connectionString: string = process.env.URI as string;
 
-const pool = new Pool({ connectionString });
+const pool: Pool = new Pool({ connectionString });
 
-pool.query(
-  `INSERT INTO status (id, message, alert) VALUES ($1,$2, $3)`,
-  [1, "1", 2],
-  (err, res) => {
-    console.log(err, res);
+// pool.query(
+//   `INSERT INTO status (id, message, alert) VALUES ($1,$2, $3)`,
+//   [1, "1", 2],
+//   (err, res) => {
+//     console.log(err, res);
+//     pool.end();
+//   }
+// );
+
+const getData = async () => {
+  try {
+    const response = await pool.query(`SELECT * from status`);
+    const data: object = response.rows[0];
     pool.end();
+    return data;
+  } catch (error) {
+    throw error;
   }
-);
+};
+
+const main = async () => {
+  var a = await getData();
+  console.log(a);
+};
+
+main();
