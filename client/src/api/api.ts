49d28f2bccdbef16709edd7950c8c8efedad4a12
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-06-18 14:44:48 +0100

    post

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -6,4 +6,13 @@ const getStatus = async () => {
   return status;
 };
 
-export { getStatus };
+const postStatus = async (message: string, alert: number) => {
+  const body: object = { pass: "a", message: message, alert: alert };
+  await fetch("https://status-web.lakeesiv.repl.co/api", {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json" },
+  });
+};
+
+export { getStatus, postStatus };
