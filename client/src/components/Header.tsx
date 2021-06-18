8f7c8f4d40ca0b10b6be55c2e66a41b4e5d6fd6d
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-06-18 14:38:02 +0100

    alert

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,13 +5,19 @@ import { getStatus } from "../api/api";
 
 const Header: React.FC = () => {
   const [status, setStatus] = useState("");
+  const [alert, setAlert] = useState(0);
 
   useEffect(() => {
-    getStatus().then((status) => setStatus(status.message));
+    getStatus().then((status) => {
+      setStatus(status.message);
+      setAlert(status.alert);
+    });
   }, []);
   return (
     <div>
-      <Heading>{status}</Heading>
+      <Heading>
+        {status}, {alert}
+      </Heading>
     </div>
   );
 };
