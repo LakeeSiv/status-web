c8d969f9491bb5f3febc65cdf19598efb4f74990
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-06-19 11:23:07 +0100

    changes

--- client/src/components/Form.tsx
+++ client/src/components/Form.tsx
@@ -1,9 +1,18 @@
-import React from "react";
-import { Input, Button, Box, VStack } from "@chakra-ui/react";
+import React, { useState } from "react";
+import {
+  Input,
+  Button,
+  Box,
+  VStack,
+  RadioGroup,
+  Stack,
+  Radio,
+} from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
 
 const Form: React.FC = () => {
   const { register, handleSubmit } = useForm();
+  const [alert, setAlert] = useState("1");
   return (
     <form>
       <VStack spacing={4} pt={4}>
@@ -13,12 +22,15 @@ const Form: React.FC = () => {
           placeholder="message"
           {...register("message")}
         />
-        <Input
-          width="50vw"
-          id="alert"
-          placeholder="alert"
-          {...register("alert")}
-        />
+        <RadioGroup onChange={setAlert} value={alert}>
+          <Stack direction="row" spacing={10}>
+            <Radio value="1">1</Radio>
+            <Radio value="2">2</Radio>
+            <Radio value="3">3</Radio>
+            <Radio value="4">4</Radio>
+            <Radio value="5">5</Radio>
+          </Stack>
+        </RadioGroup>
       </VStack>
     </form>
   );
