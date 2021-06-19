9e59412e21dfa3d0aae2e43c90b05a87cda18aad
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-06-19 16:12:31 +0100

    context provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import Header from "./components/Header";
 import Form from "./components/Form";
+import StatusContext from "./StatusContext";
 
 const App: FC = () => {
+  const [status, setStatus] = useState({ id: 1, message: "Unknown", alert: 0 });
+
   return (
     <div>
-      <Header></Header>
-      <Form></Form>
+      <StatusContext.Provider value={{ status, setStatus }}>
+        <Header></Header>
+        <Form></Form>
+      </StatusContext.Provider>
     </div>
   );
 };
