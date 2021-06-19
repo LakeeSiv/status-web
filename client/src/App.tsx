51313d8fd10d0e0e0924774cdf7f6bbd399a0f0a
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-06-19 11:12:30 +0100

    form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from "react";
 import Header from "./components/Header";
+import Form from "./components/Form";
 
 const App: FC = () => {
   return (
     <div>
       <Header></Header>
+      <Form></Form>
     </div>
   );
 };
