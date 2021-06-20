bd9904579a2b3f8ae9c42e94fd0a695d44805320
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-06-20 13:25:03 +0100

    updated theme

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -1,10 +1,19 @@
 import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+
+const styles = {
+  global: (props: any) => ({
+    body: {
+      bg: mode("gray.200", "#1A202C")(props),
+    },
+  }),
+};
 
 const config: ThemeConfig = {
   initialColorMode: "dark",
   useSystemColorMode: true,
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({ config, styles });
 
 export default theme;
