19548746948eeb2ef89ee3b7ac3daa143c3acd59
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-06-20 13:04:03 +0100

    responsiveness

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import React, { FC, useState } from "react";
+import { Box, Flex } from "@chakra-ui/layout";
 import Header from "./components/Header";
 import Form from "./components/Form";
 import StatusContext from "./StatusContext";
+import ThemePicker from "./components/ThemePicker";
 
 const App: FC = () => {
   const [status, setStatus] = useState({ id: 1, message: "Unknown", alert: 0 });
@@ -9,8 +11,19 @@ const App: FC = () => {
   return (
     <div>
       <StatusContext.Provider value={{ status, setStatus }}>
-        <Header></Header>
-        <Form></Form>
+        <ThemePicker></ThemePicker>
+        <Flex justifyContent="center">
+          <Box
+            bg="gray.700"
+            mt={5}
+            py={10}
+            width={{ lg: "60%", base: "80%" }}
+            borderRadius={8}
+          >
+            <Header></Header>
+            <Form></Form>
+          </Box>
+        </Flex>
       </StatusContext.Provider>
     </div>
   );
